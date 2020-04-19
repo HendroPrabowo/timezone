d61d0e840455eac0e7d625ba04a6c4b891ee646c
hendro <awebened@gmail.com>
2020-04-19 17:58:22 +0700

    first commit

---
+++ timezone.go
@@ -0,0 +1,13 @@
+package timezone
+
+import (
+	"time"
+)
+
+func GetIndonesiaTime() time.Time {
+	return time.Now().UTC().Add(7 * time.Hour)
+}
+
+// func GetIndonesiaTimeString() string {
+// 	return time.Now().Format("02 January 2006 15:04:05")
+// }
