1b8613f38548cd5fd3607ab44b3f6be8e72c7849
hendro <awebened@gmail.com>
2020-04-19 18:03:03 +0700

    remove comment

--- timezone.go
+++ timezone.go
@@ -8,6 +8,6 @@ func GetIndonesiaTime() time.Time {
 	return time.Now().UTC().Add(7 * time.Hour)
 }
 
-// func GetIndonesiaTimeString() string {
-// 	return time.Now().Format("02 January 2006 15:04:05")
-// }
+func GetIndonesiaTimeString() string {
+	return time.Now().Format("02 January 2006 15:04:05")
+}
